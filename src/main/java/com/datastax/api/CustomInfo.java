cbe0bd551a07bba22d553370467fae8ec99acc70
Ada Morgan <54023403+AdaMorgan@users.noreply.github.com>
2025-03-18 02:12:37 +0300

    add constants

--- src/main/java/com/datastax/api/CustomInfo.java
+++ src/main/java/com/datastax/api/CustomInfo.java
@@ -1,6 +1,10 @@
 package com.datastax.api;
 
+/**
+ * Contains information to this specific build of {@value PROJECT_NAME}.
+ */
 public class CustomInfo
 {
     public static final String PROJECT_NAME = "PROJECT_NAME";
+    public static final String PROJECT_VERSION = "0.2";
 }
