99135fb886ccd32617128aaf077002a9cc223145
Ada Morgan <54023403+AdaMorgan@users.noreply.github.com>
2025-06-11 19:48:53 +0300

    fix error

--- src/main/java/com/github/adamorgan/api/requests/objectaction/ObjectCallbackAction.java
+++ src/main/java/com/github/adamorgan/api/requests/objectaction/ObjectCallbackAction.java
@@ -2,12 +2,13 @@ package com.github.adamorgan.api.requests.objectaction;
 
 import com.github.adamorgan.api.requests.ObjectAction;
 import com.github.adamorgan.api.utils.Compression;
+import com.github.adamorgan.api.utils.binary.BinaryArray;
 import com.github.adamorgan.api.utils.request.ObjectRequest;
 import io.netty.buffer.ByteBuf;
 
 import javax.annotation.Nonnull;
 
-public interface ObjectCallbackAction extends ObjectAction<ByteBuf>, ObjectRequest<ObjectCallbackAction>
+public interface ObjectCallbackAction extends ObjectAction<BinaryArray>, ObjectRequest<ObjectCallbackAction>
 {
     @Nonnull
     ByteBuf getToken();
