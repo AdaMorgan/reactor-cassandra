7afc0f26ba151358440c2e616b427f2fd6b5e0de
Ada Morgan <54023403+AdaMorgan@users.noreply.github.com>
2025-06-13 17:35:50 +0300

    initial message

---
+++ src/main/java/com/github/adamorgan/internal/utils/tuple/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * All classes in this package have been copied from <a href="https://commons.apache.org/proper/commons-lang/" target="_blank">Lang 3</a>
+ */
+package com.github.adamorgan.internal.utils.tuple;
